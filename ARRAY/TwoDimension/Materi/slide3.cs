746a38a78c1988a4068d2f32dcddc7b913f81576
NurAkbar <91256770+SC4RECROWx@users.noreply.github.com>
2022-06-03 21:12:40 +0800

    Update slide3.cs

--- ARRAY/TwoDimension/Materi/slide3.cs
+++ ARRAY/TwoDimension/Materi/slide3.cs
@@ -19,15 +19,15 @@ namespace SandBox
 
             Console.WriteLine(grades[0, 2]);
             Console.WriteLine(ages[1, 3]);
-            
+
             string[,] names =
             {
                 {"B", "U", "D", "I" },
                 {"A", "N", "T", "O", "N"},
                 {"N", "A", "D", "I", "N", "E"},
                 {"S", "P", "E", "C", "T", "R", "E"},
-                {"M", "A", "R", "K"};
-            }
+                {"M", "A", "R", "K"}
+            };
         }
     }
 }
