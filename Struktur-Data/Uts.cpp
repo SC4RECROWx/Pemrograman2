d65e52103858ff33f504b60f7a6d42414ae7fd9c
NurAkbar <91256770+SC4RECROWx@users.noreply.github.com>
2022-06-08 23:52:53 +0800

    Update Uts.cpp

--- Struktur-Data/Uts.cpp
+++ Struktur-Data/Uts.cpp
@@ -14,25 +14,27 @@ int main(){
         getchar();
         system("cls");
         
-        int random;
-        
-        for (int i = 0; i < MAC; i++){
-            random = rand() % 9;
-            cout << random << endl;
-        }
-        int cari = random;
-        int data[MAC] = {9,6,0,5,0,9,2,3,6,7,6,1,3,2};
+        char lagi;
 
-        cout << "Data Sembarang = " << cari << endl << endl;
+        do{
+            int random = rand() % 9;
+            int cari = random;
+            int data[MAC] = {9,6,0,5,0,9,2,3,6,7,6,1,3,2};
 
-        for (int i = 0; i < MAC; i++){
-            if (cari == data[i]){
-                cout << i+1 << ". " << data[i] << " = Adalah Data Yang Dicari" << endl;
+            cout << "Data Sembarang = " << cari << endl << endl;
+
+            for (int i = 0; i < MAC; i++){
+                if (cari == data[i]){
+                    cout << i+1 << ". " << data[i] << " = Adalah Data Yang Dicari" << endl;
+                }
+                else{
+                    cout << i+1 << ". " << data[i] << " = Bukan Data Yang dicari" << endl;
+                }
             }
-            else{
-                cout << i+1 << ". " << data[i] << " = Bukan Data Yang dicari" << endl;
-            }
-        }
+
+            cout << "Ulang Program? Y/n";
+            cin  >> lagi;
+        }while(lagi == 'y' || lagi == 'Y');
 
     return 0;
 }
