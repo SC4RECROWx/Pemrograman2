e8508a760786e80d509a46354cba73a398b23910
NurAkbar <91256770+SC4RECROWx@users.noreply.github.com>
2022-06-08 21:58:46 +0800

    Update Uts.cpp

--- Struktur-Data/Uts.cpp
+++ Struktur-Data/Uts.cpp
@@ -1,1 +1,38 @@
 #include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+#define MAC 14
+using namespace std;
+
+int main(){
+    cout << endl <<"Nama  : Nur Muhammad Akbar" << endl;
+    cout << "Nim   : 20212205078" << endl;
+    cout << "Kelas : TI/208" << endl << endl;
+    cout << "Program Mencari Bilangan Sembarang" << endl;
+    cout << "Code By SC4RECROWx(NurAkbar)" << endl;
+    cout << "Press Any Key To Start....";
+    getchar();
+    system("cls");
+
+    int random;
+
+    for (int i = 0; i < MAC; i++){
+        random = rand() % 9;
+    }
+
+    int cari = random;
+    int data[MAC] = {9,6,0,5,0,9,2,3,6,7,6,1,3,2};
+
+    cout << "Data Sembarang = " << cari << endl << endl;
+
+    for (int i = 0; i < MAC; i++){
+        if (cari == data[i]){
+            cout << data[i] << " = Adalah Data Yang Dicari" << endl;
+        }
+        else{
+            cout << data[i] << " = Bukan Data Yang dicari" << endl;
+        }
+    }
+
+    return 0;
+}
