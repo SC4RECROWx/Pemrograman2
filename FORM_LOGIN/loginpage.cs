e3edc63a5f79615201b6dd2aa965224cf6183477
NurAkbar <91256770+SC4RECROWx@users.noreply.github.com>
2023-02-04 17:21:16 +0800

    Update loginpage.cs

--- FORM_LOGIN/loginpage.cs
+++ FORM_LOGIN/loginpage.cs
@@ -15,16 +15,19 @@ namespace sandbox3
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MySqlConnection conn = new MySqlConnection("SERVER = localhost; database = pt_sinar; UID = root; pwd = ''");
-            string query = "select*from user where USERNAME = '" + textBox1.Text.Trim() + "' and PASSWORD = '" + textBox2.Text.Trim() + "'";
-            MySqlDataAdapter sda = new MySqlDataAdapter(query, conn);
-            DataTable dtbl = new DataTable();
-            sda.Fill(dtbl);
-            if(dtbl.Rows.Count == 1)
+            // KONEKSI DATABASE MYSQL
+            MySqlConnection Connect = new MySqlConnection("SERVER = localhost; database = pt_sinar; UID = root; pwd = ''");
+            string Query = "select*from user where USERNAME = '" + UserName.Text.Trim() + "' and PASSWORD = '" + PassWord.Text.Trim() + "'";
+            MySqlDataAdapter DataAdp = new MySqlDataAdapter(Query, Connect);
+            DataTable data = new DataTable();
+
+            // MENGISI DATA UNTUK DIGUNAKAN SEBAGAI SYARAT LOGIN
+            DataAdp.Fill(data);
+            if(data.Rows.Count == 1)
             {
-                main objmain = new main();
+                main objMain_Program = new main();
                 this.Hide();
-                objmain.Show();
+                objMain_Program.Show();
             }
             else
             {
