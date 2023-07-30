2add53be4185850ec732032ae193aab49dd5407b
NurAkbar <91256770+SC4RECROWx@users.noreply.github.com>
2023-07-30 12:11:33 +0800

    Update tugas orang.java

--- SEMESTER4/tugas orang.java
+++ SEMESTER4/tugas orang.java
@@ -1,58 +1,60 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package perpustakaan;
 
 import java.awt.EventQueue;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.GroupLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.GroupLayout.Alignment;
 
 public class Pustakawan extends JFrame {
+
     public Pustakawan() {
-        this.initComponents();
+        initComponents();
     }
 
     private void initComponents() {
-        this.setDefaultCloseOperation(3);
-        GroupLayout layout = new GroupLayout(this.getContentPane());
-        this.getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(layout.createParallelGroup(Alignment.LEADING).addGap(0, 400, 32767));
-        layout.setVerticalGroup(layout.createParallelGroup(Alignment.LEADING).addGap(0, 300, 32767));
-        this.pack();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        JButton button = new JButton("Click Me");
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Button clicked!");
+            }
+        });
+
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(layout.createParallelGroup(Alignment.CENTER)
+            .addComponent(button)
+        );
+        layout.setVerticalGroup(layout.createParallelGroup(Alignment.CENTER)
+            .addComponent(button)
+        );
+
+        pack();
     }
 
     public static void main(String[] args) {
         try {
             UIManager.LookAndFeelInfo[] var1 = UIManager.getInstalledLookAndFeels();
-            int var2 = var1.length;
-
-            for(int var3 = 0; var3 < var2; ++var3) {
-                UIManager.LookAndFeelInfo info = var1[var3];
+            for (UIManager.LookAndFeelInfo info : var1) {
                 if ("Nimbus".equals(info.getName())) {
                     UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
-        } catch (ClassNotFoundException var5) {
-            Logger.getLogger(Pustakawan.class.getName()).log(Level.SEVERE, (String)null, var5);
-        } catch (InstantiationException var6) {
-            Logger.getLogger(Pustakawan.class.getName()).log(Level.SEVERE, (String)null, var6);
-        } catch (IllegalAccessException var7) {
-            Logger.getLogger(Pustakawan.class.getName()).log(Level.SEVERE, (String)null, var7);
-        } catch (UnsupportedLookAndFeelException var8) {
-            Logger.getLogger(Pustakawan.class.getName()).log(Level.SEVERE, (String)null, var8);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Pustakawan.class.getName()).log(Level.SEVERE, null, ex);
         }
 
         EventQueue.invokeLater(new Runnable() {
             public void run() {
-                (new Pustakawan()).setVisible(true);
+                new Pustakawan().setVisible(true);
             }
         });
     }
