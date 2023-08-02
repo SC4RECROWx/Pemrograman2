c269c45f02133808055448e566a2343de2a5095a
NurAkbar <91256770+SC4RECROWx@users.noreply.github.com>
2023-08-02 11:05:21 +0800

    Update main.cpp

--- SEMESTER4/PemodelanSimulasi/ParkingApp/main.cpp
+++ SEMESTER4/PemodelanSimulasi/ParkingApp/main.cpp
@@ -13,7 +13,7 @@ int main() {
 
     cout << "\nSIMULASI PARKING APP\nCode By Kelompok 4 Pemodelan dan Simulasi" << endl;
     cout << "1. Nur Muhammad Akbar 20212205078" << endl;
-    cout << "2. Rangga Saputra " << endl;
+    cout << "2. Rangga Saputra 20212205093" << endl;
     cout << "3. Lukmanul Hakim 20212205226" << endl;
 
     while (true) {
