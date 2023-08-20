67b67acc82f6d4ee3ccd5205b5f6767ab420c11a
NurAkbar <91256770+SC4RECROWx@users.noreply.github.com>
2023-08-20 09:20:52 +0000

    Create array.kt
    contoh sintaks array pada kotlin

---
+++ Dicoding/Kotlin/array.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val nama = arrayOf(1,2,3,4)
+    println(nama[1])
+    
+    /*\
+    untuk menginisialisasikan array
+    val mixArray = arrayOf(1,3,5,7, "Dicoding", true
+    bisa menggunakan contoh diatas
+     */
+}
