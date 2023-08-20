e2d168cf3d0f68735841d15d8971b15b324ef8cc
NurAkbar <91256770+SC4RECROWx@users.noreply.github.com>
2023-08-20 11:22:23 +0000

    Create enum.kt

---
+++ Dicoding/Kotlin/enum.kt
@@ -0,0 +1,15 @@
+fun main(){
+    val colorRed = Color.RED
+    val colorGreen = Color.GREEN
+    val colorBlue = Color.BLUE
+
+    println(colorGreen)
+    println(colorRed)
+    println(colorBlue)
+}
+
+enum class Color(val value: Int){
+    RED(0xFF0000),
+    GREEN(0x00FF00),
+    BLUE(0x0000FF)
+}
