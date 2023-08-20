dad43acb64fda15b4c6371dbf2fd02173fd587b9
NurAkbar <91256770+SC4RECROWx@users.noreply.github.com>
2023-08-20 07:52:16 +0000

    Create Variable.kt
    testing variabel

---
+++ Dicoding/Kotlin/Variable.kt
@@ -0,0 +1,16 @@
+fun main(){
+    val maxInt = Int.MAX_VALUE
+    val minInt = Int.MIN_VALUE
+    val overRangeInt = Int.MAX_VALUE + 2
+
+    println(maxInt)
+    println(minInt)
+    println(overRangeInt)
+    
+    /*
+    KESIMPULANNYA ADALAH KETIKA NILAI MELEWATI
+    BATAS MAX VARIABLE MAKA AKAN TERJADI LOOPING
+    YANG ARTINYA AKAN KEMBALI KE NILAI MIN, BEGITU JUGA
+    DENGAN MIN AKAN KEMBALI KE MAX
+     */
+}
