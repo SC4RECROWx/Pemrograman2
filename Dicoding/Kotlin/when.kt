ffe61c97f9111ab2bcd5ff930c792423a2a0cbac
NurAkbar <91256770+SC4RECROWx@users.noreply.github.com>
2023-08-21 20:17:15 +0800

    Update when.kt

--- Dicoding/Kotlin/when.kt
+++ Dicoding/Kotlin/when.kt
@@ -11,3 +11,75 @@ fun main(){
 
     println(stringOfValue)
 }
+
+// CONTOH2
+val value = 7
+    val stringOfValue = when (value) {
+        6 -> {
+            println("Six")
+            "value is 6"
+        }
+        7 -> {
+            println("Seven")
+            "value is 7"
+        }
+        8 -> {
+            println("Eight")
+            "value is 8"
+        }
+        else -> {
+            println("undefined")
+            "value cannot be reached"
+        }
+    }
+ 
+    println(stringOfValue)
+}
+ 
+/*
+   output : 
+            Seven
+            value is 7
+ 
+*/
+
+// CONTOH 3
+    fun main() {
+        val anyType : Any = 100L
+        when(anyType){
+            is Long -> println("the value has a Long type")
+            is String -> println("the value has a String type")
+            else -> println("undefined")
+        }
+    }
+     
+    /*
+       output : the value has a Long type
+    */
+
+// CONTOH 4
+    fun main() {
+        val value =  27
+        val ranges = 10..50
+     
+        when(value){
+            in ranges -> println("value is in the range")
+            !in ranges -> println("value is outside the range")
+            else -> println("value undefined")
+        }
+    }
+     
+    /*
+       output : value is in the range
+    */
+
+// CONTOH 5
+    fun main() {
+        val registerNumber = when(val regis = getRegisterNumber()){
+            in 1..50 -> 50 * regis
+            in 51..100 -> 100 * regis
+            else -> regis
+        }
+    }
+     
+    fun getRegisterNumber() = Random.nextInt(100)
