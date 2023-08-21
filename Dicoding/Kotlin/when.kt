b690ffe8a9ff1af5adcabda716988f158ce3bdf3
NurAkbar <91256770+SC4RECROWx@users.noreply.github.com>
2023-08-21 19:54:52 +0800

    Create when.kt

---
+++ Dicoding/Kotlin/when.kt
@@ -0,0 +1,13 @@
+package com.dicoding.kotlin
+
+fun main(){
+    val value = 7
+    val stringOfValue = when(value){
+        6 -> "Value is 6"
+        7 -> "Value is 7"
+        8 -> "Value is 8"
+        else -> "Value is not reached"
+    }
+
+    println(stringOfValue)
+}
